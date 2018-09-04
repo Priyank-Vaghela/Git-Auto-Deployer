5ca1c89f5dca0279744a31661fc8a3ff68df96eb
Priyank Vaghela <19762938+Priyank-Vaghela@users.noreply.github.com>
2018-09-04 15:36:08 +0530

    Update git_auto_deploy.sh

--- git_auto_deploy.sh
+++ git_auto_deploy.sh
@@ -9,5 +9,6 @@ git remote remove origin
 git remote add origin $repo
 git add .
 git commit -am "Update $(git status | grep modified | sed 's/modified://g' | sed 's/ //g' | tr -d "\n")"
+git config --global credential.helper store
 git push origin master
-git status | grep modified
+echo Your code is deployed successfully.
