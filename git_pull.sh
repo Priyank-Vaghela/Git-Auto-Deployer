15a0230c9bc5f86826cae7ed31548fc3eb549f2d
Priyank Vaghela <19762938+Priyank-Vaghela@users.noreply.github.com>
2018-09-04 15:29:00 +0530

    Update git_pull.sh

--- git_pull.sh
+++ git_pull.sh
@@ -1,10 +1,11 @@
 echo Initializing Git Puller..
+read -p "Enter Your Repository Name: "  name_repo
 read -p "Enter Your Repository URL: "  repo
-echo "Adding your Repo.. $repo!"
-mkdir Pulled_code
-cd Pulled_code
+echo "Adding your Repo..."
+mkdir $name_repo
+cd $name_repo
 git init
 git remote add origin $repo
 git add .
-git pull origin master
-echo Your code is now Pulled in your System..:)
+git pull origin master --force
+echo Your code is successfully pulled in your System.
