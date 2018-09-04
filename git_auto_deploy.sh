292af12ffa608ca81a3b0fadc0320fddea4db562
Priyank Vaghela <19762938+Priyank-Vaghela@users.noreply.github.com>
2018-09-04 15:13:20 +0530

    Update git_auto_deploy.sh

--- git_auto_deploy.sh
+++ git_auto_deploy.sh
@@ -1,4 +1,10 @@
+echo Initializing Git auto deployer..
+read -p "Enter Your Repository URL: "  repo
+echo "Adding your Repo.. $repo!"
+git init
+git remote remove origin
+git remote add origin $repo
 git add .
-git commit -am "Update $(git status | grep modified | sed 's/modified://' | tr -d "\n" | tr -d " ")"
-git push origin dev
+git commit -am "Update $(git status | grep modified | sed 's/modified://g' | sed 's/ //g' | tr -d "\n")"
+git push origin master
 git status | grep modified
