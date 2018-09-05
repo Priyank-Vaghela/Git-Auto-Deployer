8c21f6b5ee9f2df3520a05cd596eba3c243fbef1
Priyank <priyankvaghela11@gmail.com>
2018-09-05 15:24:20 +0530

    Added Windows Support

---
+++ For Windows/Pro tools/git_1-Click_Pull.bat
@@ -0,0 +1,10 @@
+echo Initializing Git Puller..
+read -p "Enter Your Repository Name: "  name_repo
+read -p "Enter Your Repository URL: "  repo
+echo "Adding your Repo..."
+mkdir $name_repo
+cd $name_repo
+git init
+git remote add origin $repo
+git add .
+git pull origin master --force
