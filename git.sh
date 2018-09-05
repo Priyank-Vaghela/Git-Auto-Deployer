0a8f9cefa4dda20f115a1cae4acc0757df75d7c9
Priyank Vaghela <19762938+Priyank-Vaghela@users.noreply.github.com>
2018-09-05 10:09:23 +0530

    Update git.sh

--- git.sh
+++ git.sh
@@ -2,4 +2,3 @@ git add .
 git commit -am "Update $(git status | grep modified | sed 's/modified://g' | sed 's/ //g' | tr -d "\n")"
 git config --global credential.helper store
 git push origin master
-echo Your code is deployed successfully.
