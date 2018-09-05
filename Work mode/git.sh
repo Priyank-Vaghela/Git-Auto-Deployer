4501cf6364479b9cf587c534b637191a67cbe01a
Priyank Vaghela <19762938+Priyank-Vaghela@users.noreply.github.com>
2018-09-05 15:12:58 +0530

    Delete git.sh

--- Work mode/git.sh
+++
@@ -1,4 +0,0 @@
-git add .
-git commit -m "Update $(git status | grep modified | sed 's/modified://g' | sed 's/ //g' | tr -d "\n")"
-git config --global credential.helper store
-sudo git push origin master
